4c7c266b77a9e9fb8ccb7806d18bb96043fb5daa
John Campoverde <jacampoverde421@gmail.com>
2025-08-24 14:27:17 -0400

    Post Dot Revision

--- scripts/index.js
+++ scripts/index.js
@@ -20,40 +20,45 @@ const newPostCaptionInput = newPostModal.querySelector("#new-caption-input");
 const profileNameEl = document.querySelector(".profile__name");
 const profileDescriptionEl = document.querySelector(".profile__description");
 
-const newPostLinkEl = document.querySelector(".card__image");
-const newPostCaptionEl = document.querySelector(".card__title");
+function openModal(modal) {
+  modal.classList.add("modal_is-opened");
+}
+
+function closeModal(modal) {
+  modal.classList.remove("modal_is-opened");
+}
 
 editProfileBtn.addEventListener("click", function () {
   editProfileDescriptionInput.value = profileDescriptionEl.textContent;
   editProfileNameInput.value = profileNameEl.textContent;
-  editProfileModal.classList.add("modal_is-opened");
+  openModal(editProfileModal);
 });
 
 editProfileCloseBtn.addEventListener("click", function () {
-  editProfileModal.classList.remove("modal_is-opened");
+  closeModal(editProfileModal);
 });
 
 newPostBtn.addEventListener("click", function () {
-  newPostModal.classList.add("modal_is-opened");
+  openModal(newPostModal);
 });
 
 newPostCloseBtn.addEventListener("click", function () {
-  newPostModal.classList.remove("modal_is-opened");
+  closeModal(newPostModal);
 });
 
 function handleEditProfileSubmit(evt) {
   evt.preventDefault();
   profileNameEl.textContent = editProfileNameInput.value;
   profileDescriptionEl.textContent = editProfileDescriptionInput.value;
-  editProfileModal.classList.remove("modal_is-opened");
+  closeModal(editProfileModal);
 }
 
 editProfileForm.addEventListener("submit", handleEditProfileSubmit);
 
 function handleNewPostSubmit(evt) {
   evt.preventDefault();
-  newPostLinkEl.src = newPostLinkInput.value;
-  newPostCaptionEl.textContent = newPostCaptionInput.value;
-  newPostModal.classList.remove("modal_is-opened");
+  console.log("Link:", newPostLinkInput.value);
+  console.log("Caption:", newPostCaptionInput.value);
+  closeModal(newPostModal);
 }
 newPostForm.addEventListener("submit", handleNewPostSubmit);
